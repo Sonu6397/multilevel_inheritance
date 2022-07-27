d3e8df746dc681da1454208e253d7fb5fe3ab4b9
admin <admin@CHETUIWK1860>
2022-07-27 12:20:40 +0530

    Add project files.

---
+++ multilevel_inheritance/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace multilevel_inheritance
+{
+     class car
+    {
+        public string model;
+        public int price;
+        public string color;
+
+        public void printcarinfo()
+        {
+            Console.WriteLine("model="+model);
+            Console.WriteLine("price="+price);
+            Console.WriteLine("color="+color);
+        }
+    }
+
+    class farrari : car
+    {
+        public int speed;
+
+        public void printspeed()
+        {
+            Console.WriteLine("speed="+speed);
+        }
+    }
+
+    class maruti : farrari
+    {
+        public int mileage;
+        public void printmileage()
+        {
+            Console.WriteLine("mileage=" + mileage);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            maruti obj = new maruti();
+            obj.model = "BMw";
+            obj.price = 20000;
+            obj.color = "black";
+            obj.speed = 100;
+            obj.mileage = 22;
+            obj.printcarinfo();
+            obj.printspeed();
+            obj.printspeed();
+            Console.ReadLine();
+        }
+    }
+}
